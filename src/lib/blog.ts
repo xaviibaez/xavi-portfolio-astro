47e849ec6544381171d4d86c288c58e1cbe80f88
Xavi Ibáñez Martínez <241xavi@gmail.com>
2026-04-26 21:52:47 +0200

    Refactor getBlogStaticPaths extrating method

---
+++ src/lib/blog.ts
@@ -0,0 +1,12 @@
+import { getCollection } from 'astro:content';
+import type { Lang } from '@/i18n/utils';
+
+export async function getBlogStaticPaths(lang: Lang) {
+  const posts = await getCollection('blog');
+  return posts
+    .filter((p) => p.id.startsWith(`${lang}/`))
+    .map((post) => ({
+      params: { slug: post.id.split('/').pop() },
+      props: { post },
+    }));
+}
